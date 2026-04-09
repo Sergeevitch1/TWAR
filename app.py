fb88dd19506f7058be4da78ad0532f3cdd036601
Sergeevitch1 <166446866+Sergeevitch1@users.noreply.github.com>
2026-04-09 13:37:51 +0300

    app.py

--- app.py
+++ app.py
@@ -221,8 +221,10 @@ def create_app():
 
     return app
 
+# 👇 ВАЖНО: глобально для gunicorn
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
     app.run(
         host='0.0.0.0',
         port=5000,
